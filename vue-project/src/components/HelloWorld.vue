43683511e55bb654b7302fc76a714ee5e4d2ee81
erycaaaaa <eryca847@gmail.com>
2025-11-09 12:01:57 +0700

    Rename to RUart & refresh tagline
    feat(branding)
    1. rename to RUart
    2. update site description
    3. replace logo

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Brushstrokes write what words can’t. By
+      <a href="https://portfolio-erycadhamma.vercel.app/" target="_blank" rel="noopener">ErycaDS</a> 
+      Nice to meet you!
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
